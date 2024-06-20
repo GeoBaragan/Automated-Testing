facd0990be1a110abb5dc751f16cf6f39ba576f1
GeoBaragan <baragangeorgiana98@yahoo.com>
2024-06-20 18:03:35 +0300

    Teste Portofoliu

--- cypress/e2e/Teste portofoliu/TestePortofoliu.cy.js
+++ cypress/e2e/Teste portofoliu/TestePortofoliu.cy.js
@@ -1,23 +1,63 @@
-// describe('Login', () => {
-//     // it('Error message', () => {
-//     //     cy.visit('https://www.saucedemo.com/');
-//     //     cy.get('.user-name').click();
-//     //     cy.get('.user-name').type('standard_user');
-//     //     cy.get('.password').type('standard');
-//     //     cy.get('.login-button').click();
+describe('Login', () => {
+    it('Error message', () => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('standard');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="error"]').should('exist');
+        cy.get('[data-test="error"]').contains('Epic sadface: Username and password do not match any user in this service');
+        cy.get('[data-test="error"]').contains('Epic sadface: Username and password do not match any user in this service').should('exist');
+
+    })
 
 
+    it('Login reusit', () => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
 
-//     // })
+})
+
+    it('Logout reusit', () => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('[data-test="logout-sidebar-link"]').click();     
+
+    })
+
+    it('Deschidere si inchidere meniu lateral', () => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#react-burger-cross-btn').click();
 
 
-// it('Login reusit', () => {
-//     cy.visit('https://www.saucedemo.com/');
-//     cy.get('.user-name').click();
-//     cy.get('.user-name').type('standard_user');
-//     cy.get('.password').type('secret_sauce');
-//     cy.get('.login-button').click();
+    })
+
+    it('Adaugare produs in cos', () => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+        cy.get('[data-test="shopping-cart-link"]').click();
+        cy.get('[data-test="inventory-item-name"]').should('exist');
+
+    })
+
+    it('Eliminare produs din cos', () => {
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="shopping-cart-link"]').click();
+        
 
 
-// })
-// })
+    })
+})
